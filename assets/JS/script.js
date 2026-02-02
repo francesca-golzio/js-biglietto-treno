13e110e8267372b79ecd1e6d49613848a7074c3f
Francesca Golzio <francescagolzio@gmail.com>
2026-02-02 16:26:09 +0100

    add some computational thoughts

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,3 +1,38 @@
 // Controllo: funziona? vedo qualcosa in console? ho collegato file JS eHTML?
 console.log("It's working");
 
+/* WHAT WE NEED:
+  - variables
+  - prompt()
+  - JS operators
+  - if/else
+*/
+
+// Aks User for kilometers amount
+
+  // Store value in a variable
+  
+// Aks User for kilometers amount
+  
+  // Store value in a variable
+  
+// Determin total ticket price amount
+
+  // Calc starting price based on km
+
+  // Check if the User classifies for a discount
+
+    // IF underage -> apply 20% discount
+
+      // Store the price value in a var
+      
+    // IF/ELSE over 65 -> apply 40% discount
+      
+      // Store the price value in a var
+      
+    // ELSE do not apply discount      
+      // Store the price value in a var
+    
+  // Convert final price into a human friendly form: no more than 2 decimal places
+  
+
