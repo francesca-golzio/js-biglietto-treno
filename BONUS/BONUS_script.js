4ec7c4112dc39923c4457925d3e64a3df3f79222
Francesca Golzio <francescagolzio@gmail.com>
2026-02-02 17:54:45 +0100

    start BONUS project
    Intended goal from now on: build and implement an in-page grafical frame for the app

---
+++ BONUS/BONUS_script.js
@@ -0,0 +1,66 @@
+// Controllo: funziona? vedo qualcosa in console? ho collegato file JS e HTML?
+console.log("It's working");
+
+/* WHAT WE NEED:
+  - variables
+  - JS operators
+  - prompt()
+  - if/else
+  - Number()
+  - toFixed()
+*/
+
+// Collect data
+let kilometers, price_km_based, output_price, user_price, age
+
+  // Aks User for kilometers amount - PROMPT()
+  // convert from string to number - NUMBER()
+  // store the value in a variable
+  kilometers = Number(prompt('How many kilometers long is your railway route?')); 
+  // console.log(kilometers)
+
+  // Aks User for traveler age - PROMPT
+  // convert from string to number - NUMBER()
+  // store the value in a variable
+  age = Number(prompt('How old is the passenger?'));  
+  // console.log(age)    
+    
+
+// Determin total ticket price amount
+
+  // Calc starting price based on km 
+  // (and store the value in a variable)
+  price_km_based = (kilometers * 0.21); 
+  // console.log(price_km_based)
+
+  // Check if the User classifies for a discount 
+  // (and eventually apply it)
+
+  if (age < 18) {
+      // IF underage (see above)
+      // apply 20% discount 
+      // (and store the value in a variable)
+      output_price = (price_km_based - (price_km_based / 100) * 20);
+
+    } else if (age >= 65) {
+        // ELSE/IF over 65 (see above)
+        // apply 40% discount 
+        // (and store the value in a variable)
+        output_price = (price_km_based - (price_km_based / 100) * 40);
+
+    } else {      
+      // ELSE do not apply discount 
+      // (and store the value in a variable)
+      output_price = price_km_based;
+    }
+    //console.log(output_price)
+
+  // Convert final price into a human friendly form: 
+  // no more than 2 decimal places 
+  // (and store value)
+  user_price = output_price.toFixed(2);  
+  //console.log(user_price)
+
+
+  
+
